23963af15234d1dc6c86dc5efc9f7bee8c3bd5d0
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-08-31 13:07:02 -0400

    Changed the way I address solidBar function to use arg side to simplify

--- turnsignal.ino
+++ turnsignal.ino
@@ -1,32 +1,44 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6 //I may need to adjust this for production hardware when I move to nano or micro
+#define PIN0 6 //LEFT SIDE I may need to adjust this for production hardware when I move to nano or micro
+#define PIN1 7 //RIGHT SIDE lightbar
 #define NUMPIXELS 8 //the number of pixels being used. Make sure to update this if I end up chaining the strips
-#define SWITCH0 3 //make sure this is ok, used for direction
-#define SWITCH1 4 //make sure this is ok, used for direction
+#define SWITCH0 3 //LEFT SIDE make sure this is ok, used for direction
+#define SWITCH1 4 //RIGHT SIDEmake sure this is ok, used for direction
 #define BUTTON0 2 //this is for the mode-switcher, make sure this is ok
 
-Adafruit_Neopixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+// Timing
+#define time0 1000 //1 second is the default wait time
+
+Adafruit_Neopixel pixels0(NUMPIXELS, PIN0, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels1(NUMPIXELS, PIN1,  NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-    pixels.begin();             //initializes the pixels
-    pixels.show();              //turns brightness off
-    pixels.setBrightness(200);  //sets the brightness (max 255)
+    pixels0.begin();             //initializes the pixels
+    pixels1.begin();
+    pixels0.show();  
+    pixels1.show();            //turns brightness off
+    pixels0.setBrightness(200);  //sets the brightness (max 255)
+    pixels1.setBrightness(200);
 }
 
 void loop() {
-    if(SWITCH0=HIGH) {
-
+    while(SWITCH0=HIGH) do{
+        solidBar(pixels0);
     }
 
 }
 
-void solidBar(int wait) {
-    pixels.clear();
+void solidBar(int side) { //Do I want to use int wait?? Figure out exactly what it does
+    side.clear();
     for(int i=0; i<NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(255,0,0));
-        pixels.show();
+        side.setPixelColor(i, side.Color(255,0,0));
+        side.show();
     }
-    delay(1000);
+    delay(time0);
+    side.clear();//is this redundant? Test. 
+    side.show(); //is this redundant? Test. 
+    delay(time0);
 }
+
