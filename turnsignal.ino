5ab67e99a70aea86f75b10bd0e0d079bedaa914d
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-08-31 12:52:42 -0400

    more work on turnsignal.ino. Started actual coding.

--- turnsignal.ino
+++ turnsignal.ino
@@ -2,6 +2,9 @@
 
 #define PIN 6 //I may need to adjust this for production hardware when I move to nano or micro
 #define NUMPIXELS 8 //the number of pixels being used. Make sure to update this if I end up chaining the strips
+#define SWITCH0 3 //make sure this is ok, used for direction
+#define SWITCH1 4 //make sure this is ok, used for direction
+#define BUTTON0 2 //this is for the mode-switcher, make sure this is ok
 
 Adafruit_Neopixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -13,6 +16,17 @@ void setup() {
 }
 
 void loop() {
+    if(SWITCH0=HIGH) {
 
+    }
 
+}
+
+void solidBar(int wait) {
+    pixels.clear();
+    for(int i=0; i<NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(255,0,0));
+        pixels.show();
+    }
+    delay(1000);
 }
